b038d7ed01fa876fa442e41b39940ec96fa0433c
jimmypark44 <71073823+jimmypark44@users.noreply.github.com>
2021-04-28 09:20:01 +0900

    feat: login

--- src/routers/userController.ts
+++ src/routers/userController.ts
@@ -1,10 +1,13 @@
-import express, { RequestHandler } from "express";
+import express, { RequestHandler, response } from "express";
 import Controller from "./interfaces/controller";
 import User from "../models/User/interface";
 import UserModel from "../models/User/model";
 import validation from "../middlewares/validation";
 import UserDto from "../models/User/dto";
+import "dotenv/config"
 import { Types } from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 class UserController implements Controller {
   public path = "/auth";
@@ -22,6 +25,9 @@ class UserController implements Controller {
       validation(this.dto, true),
       this.updateUser
     );
+    // dto 따로 안만들어도 되나?
+    this.router.post(`${this.path}/login`, validation(this.dto), this.login);
+
   }
 
   private createUser: RequestHandler = async (req, res, next) => {
@@ -55,6 +61,27 @@ class UserController implements Controller {
       next(err);
     }
   };
+
+  // 회원가입할떄 비밀번호 해쉬해주기, res.setHeader??
+  private login: RequestHandler = async (req, res, next) => {
+    const userLoginData: User = req.body;
+    try {
+      const user = await this.user.findOne({ id: userLoginData.id })
+      if (!user)
+        next(new Error("아이디 입력이 잘못되었습니다"))
+      if (user) {
+        const passwordMatch = await bcrypt.compare(userLoginData.password, user.password)
+        if (!passwordMatch)
+          next(new Error("비밀번호가 일치하지 않습니다"))
+        const secret = process.env.TOKEN_KEY
+        const token = jwt.sign({ userId: user._id }, secret)
+        res.send({ result: { user: { token: token } } })
+      }
+    } catch (err) {
+      console.log(err)
+      next(err)
+    }
+  }
 }
 
 export default UserController;
