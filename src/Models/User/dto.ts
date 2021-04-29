f6b105dc52c1a10068e9da7d364340149f91e643
jimmypark44 <71073823+jimmypark44@users.noreply.github.com>
2021-04-29 21:49:18 +0900

    fix: createUser

--- src/Models/User/dto.ts
+++ src/Models/User/dto.ts
@@ -2,15 +2,7 @@ import { IsString } from "class-validator";
 
 class UserDto {
   @IsString()
-  readonly id: string;
-  @IsString()
-  readonly password: string;
-  @IsString()
-  readonly nickname: string;
-  @IsString()
   readonly name: string;
-  @IsString()
-  readonly phone: string;
 }
 
 export default UserDto;
--- src/Models/User/model.ts
+++ src/Models/User/model.ts
@@ -3,9 +3,6 @@ import User from "./interface";
 
 const UserSchema = new Schema(
   {
-    id: { type: String, required: true },
-    password: { type: String, required: true },
-    nickname: { type: String, required: true },
     name: { type: String, required: true },
     phone: { type: String, required: true },
     userImg: { type: String },
