dec14917fe6405d34cb22936b963885dd6aa1895
YOOJIYUN <ys7638@naver.com>
2021-05-01 16:42:08 +0900

    fix: app.ts setError

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-import express, { Request, Response, NextFunction } from "express"
-import mongoose from "mongoose"
-import cors from "cors"
-import Controller from "./routers/interfaces/controller"
-import "dotenv/config"
+import express, { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import Controller from "./routers/interfaces/controller";
+import "dotenv/config";
 
 class App {
-  app: express.Application
+  app: express.Application;
   constructor(controllers: Controller[]) {
-    this.app = express()
-    this.setDB()
-    this.setMiddleWare()
-    this.setRouter(controllers)
-    this.set404Error()
-    this.setError()
+    this.app = express();
+    this.setDB();
+    this.setMiddleWare();
+    this.setRouter(controllers);
+    this.set404Error();
+    this.setError();
   }
   private setDB() {
     mongoose
@@ -26,32 +26,34 @@ class App {
         // pass: process.env.DB_PASSWORD,
       })
       .then(() => console.log("db connected"))
-      .catch((err) => console.log(err))
+      .catch((err) => console.log(err));
   }
   private setMiddleWare() {
-    this.app.use(cors())
-    this.app.use(express.urlencoded({ extended: false }))
-    this.app.use(express.json())
+    this.app.use(cors());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(express.json());
   }
   private setRouter(controllers: Controller[]) {
     this.app.get("/", (req, res) => {
-      res.send("hello")
-    })
+      res.send("hello");
+    });
     controllers.forEach((controller) => {
-      this.app.use("/", controller.router)
-    })
+      this.app.use("/", controller.router);
+    });
   }
   private set404Error() {
     this.app.use((req, res, _) => {
-      res.status(404).send("404")
-    })
+      res.status(404).send("404");
+    });
   }
   private setError() {
-    this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-      console.log(err)
-      res.status(500).send(err.message)
-    })
+    this.app.use(
+      (err: Error, req: Request, res: Response, next: NextFunction) => {
+        console.log(err);
+        res.status(500).send({ err: err.message });
+      }
+    );
   }
 }
 
-export default App
+export default App;
