8e6658e385d323ddbd89b68a9df52a17688d9c69
YOOJIYUN <ys7638@naver.com>
2021-05-03 16:02:58 +0900

    fix: posts model&interface

--- src/Models/Post/model.ts
+++ src/Models/Post/model.ts
@@ -15,6 +15,7 @@ const PostSchema = new Schema(
     location: [Number],
     startDate: { type: Date },
     dueDate: { type: Date },
+    participants: { type: [{ type: Types.ObjectId, ref: "user" }] },
   },
   {
     timestamps: true,
