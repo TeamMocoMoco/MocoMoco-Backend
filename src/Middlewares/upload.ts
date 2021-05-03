f439cceb2978a57d3c591d937e3bfb3c27c7aac0
kks <keumks789@naver.com>
2021-05-04 00:42:32 +0900

    fix : add name in upload

--- src/Middlewares/upload.ts
+++ src/Middlewares/upload.ts
@@ -4,13 +4,13 @@ import multerS3 from 'multer-s3'
 import path from 'path'
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.LOVE_S3_ID,
-  secretAccessKey: process.env.LOVE_S3_PW,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
 })
 
 const S3storage = multerS3({
   s3,
-  bucket: "",
+  bucket: process.env.BUCKET_NAME ||"test",
   key(req, file, cb) {
     cb(null, `original/${Date.now()}${path.basename(file.originalname)}`)
   },
@@ -29,3 +29,5 @@ const serverStorage = multer.diskStorage({
 const upload = multer({
   storage: S3storage,
 })
+
+export default upload
