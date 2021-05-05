374cd690922d56ce7c4b9200cfe1d07f6e25c7c1
YOOJIYUN <ys7638@naver.com>
2021-05-05 23:04:21 +0900

    fix: postService keyword search

--- src/routers/Post/config.ts
+++ src/routers/Post/config.ts
@@ -1,5 +1,2 @@
 export type Meeting = "온라인" | "오프라인";
 export const userInfo = "name role userImg";
-export function keywordOption(keyword: string) {
-  return { title: { $regex: keyword }, hashtag: { $regex: keyword } };
-}
