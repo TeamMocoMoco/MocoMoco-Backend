8beac4c463cd00df90741aa543757f717e55e6e8
YOOJIYUN <ys7638@naver.com>
2021-05-07 00:21:47 +0900

    fix: scheduleService

--- src/routers/Post/scheduleService.ts
+++ src/routers/Post/scheduleService.ts
@@ -9,8 +9,7 @@ export default class ScheduleService {
   changeStatus = async (): Promise<void> => {
     const date = Date.now();
     const real_date = new Date(date);
-    real_date.setDate(real_date.getHours() + 9);
-    console.log(new Date(date));
+    real_date.setHours(real_date.getHours() + 9);
     await this.post.updateMany(
       { startDate: { $lte: real_date } },
       { $set: { status: false } }
