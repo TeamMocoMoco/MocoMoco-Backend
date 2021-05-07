11f8c18bac9814ab5dead9a3584508c2260b318d
jimmypark44 <71073823+jimmypark44@users.noreply.github.com>
2021-05-07 12:24:31 +0900

    fix: getLocationSearch doesn't need location anymore + added error handles

--- src/routers/Post/mapService.ts
+++ src/routers/Post/mapService.ts
@@ -2,64 +2,70 @@ import { Post, PostModel } from "../../models/Post";
 const axios = require('axios').default;
 
 class MapService {
-    private post = PostModel;
-    constructor() { }
+  private post = PostModel;
+  constructor() { }
 
-    getLocationSearch = async (
-        location: string,
-        keyword: string
-    ): Promise<any> => {
-        try {
-            const response = await axios
-                .get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=1500&language=ko&keyword=`
-                    + encodeURI(keyword) + `&key=${process.env.GOOGLE_API_KEY}`);
-            return response
-        } catch (err) {
-            throw new Error(err)
-        }
+  getLocationSearch = async (
+    keyword: string
+  ): Promise<any> => {
+    try {
+      const findPlaceFromText = await axios
+        .get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery&fields=geometry&input=`
+          + encodeURI(keyword) + `&key=${process.env.GOOGLE_API_KEY}`)
+      if (!findPlaceFromText.data.candidates[0]) throw new Error("검색 결과가 없습니다")
+      const locationLat = findPlaceFromText.data.candidates[0].geometry.location.lat
+      const locationLng = findPlaceFromText.data.candidates[0].geometry.location.lng
+      const coordinate = `${locationLat}` + ',' + `${locationLng}`
+      const response = await axios
+        .get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coordinate}&radius=5000&language=ko&keyword=`
+          + encodeURI(keyword) + `&key=${process.env.GOOGLE_API_KEY}`);
+      return response
+    } catch (err) {
+      throw new Error(err)
     }
+  }
 
-    getLocationToken = async (token: string): Promise<any> => {
-        try {
-            const response = await axios
-                .get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=${token}&key=${process.env.GOOGLE_API_KEY}`)
-            return response
-        } catch (err) {
-            throw new Error(err)
-        }
+  getLocationToken = async (token: string): Promise<any> => {
+    try {
+      const response = await axios
+        .get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=${token}&key=${process.env.GOOGLE_API_KEY}`)
+      return response
+    } catch (err) {
+      throw new Error(err)
     }
+  }
 
-    getBounds = async (LatLng: string): Promise<{ Lat: number, Lng: number }> => {
-        try {
-            // LatLng = 11.111,22.222
-            const indexOfComma = LatLng.indexOf(",");
+  getBounds = async (LatLng: string): Promise<{ Lat: number, Lng: number }> => {
+    try {
+      // LatLng = 11.111,22.222
+      const indexOfComma = LatLng.indexOf(",");
 
-            const Lat: number = +LatLng.slice(0, indexOfComma)
-            const Lng: number = +LatLng.substr(indexOfComma + 1)
-            return { Lat, Lng }
-        } catch (err) {
-            throw new Error(err)
-        }
+      const Lat: number = +LatLng.slice(0, indexOfComma)
+      const Lng: number = +LatLng.substr(indexOfComma + 1)
+      return { Lat, Lng }
+    } catch (err) {
+      throw new Error(err)
     }
+  }
 
-    getPostsInMap = async (
-        sBound: number,
-        nBound: number,
-        wBound: number,
-        eBound: number
-    ): Promise<Post[]> => {
-        try {
-            const posts = await this.post.find({
-                $and: [
-                    { "location.0": { $gt: sBound, $lt: nBound } },
-                    { "location.1": { $gt: wBound, $lt: eBound } },
-                ],
-            });
-            return posts;
-        } catch (err) {
-            throw new Error(err);
-        }
-    };
+  getPostsInMap = async (
+    sBound: number,
+    nBound: number,
+    wBound: number,
+    eBound: number
+  ): Promise<Post[]> => {
+    try {
+      const posts = await this.post.find({
+        $and: [
+          { "location.0": { $gt: sBound, $lt: nBound } },
+          { "location.1": { $gt: wBound, $lt: eBound } },
+        ],
+      });
+      return posts;
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
 
 }
 
