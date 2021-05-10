c7dccfe757d13f4db1eca1023ab8d412ad388eb0
YOOJIYUN <ys7638@naver.com>
2021-05-10 20:46:01 +0900

    add: test

--- src/Test/RouterTest/posts.test.ts
+++ src/Test/RouterTest/posts.test.ts
@@ -1,19 +1,22 @@
-import mongoose from "mongoose"
-import { dropAllCollections, removeAllCollections } from "../test-setup"
-import PostService from "../../routers/Post/postService"
-const databaseName = "test"
+import mongoose from "mongoose";
+import { dropAllCollections, removeAllCollections } from "../test-setup";
+import PostService from "../../routers/Post/postService";
+const databaseName = "test";
 
 beforeAll(async () => {
-  const url = `mongodb://127.0.0.1/${databaseName}`
-  await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-})
+  const url = `mongodb://127.0.0.1/${databaseName}`;
+  await mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
 
 describe("postService test", () => {
-  const postService = new PostService()
-  test("포스트 생성", () => {})
-})
+  const postService = new PostService();
+  test("포스트 생성", async () => {});
+});
 
 afterAll(async () => {
-  await mongoose.connection.close()
-  await dropAllCollections()
-})
+  await mongoose.connection.close();
+  await dropAllCollections();
+});
