c77681db136a53605edf746605378c59ec5e0add
YOOJIYUN <ys7638@naver.com>
2021-05-12 17:27:01 +0900

    refactor: schedule Middleware

--- src/Middlewares/schedule.ts
+++ src/Middlewares/schedule.ts
@@ -1,26 +1,7 @@
 import * as schedule from "node-schedule";
 import ScheduleService from "../routers/Post/scheduleService";
 
-interface scheduleWork {
-  hour?: number;
-  minute?: number;
-  dayOfWeek?: number;
-}
-
-const dayOfWork: scheduleWork = {
-  hour: 22,
-  minute: 15,
-  dayOfWeek: 6,
-};
-
 const scheduleService = new ScheduleService();
-
-// test용 scheduler
-// const startTime = new Date(Date.now() + 5000);
-// const endTime = new Date(startTime.getTime() + 5000);
-// export const test = schedule.scheduleJob("10 * * * * *", () => {
-//   console.log("test");
-// });
 
 //사용하는 scheduler
 export const scheduleJob = schedule.scheduleJob("59 14 * * *", async () => {
