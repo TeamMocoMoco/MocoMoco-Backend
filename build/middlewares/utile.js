a2174bb98a1aa63c31d3b0f6359840c061fc56f1
YOOJIYUN <ys7638@naver.com>
2021-05-14 16:55:29 +0900

    remove: build file

--- build/middlewares/utile.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rand = void 0;
-function rand(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-exports.rand = rand;
