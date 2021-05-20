e5b2e1619a30c765ac1cc87500e76446a95c3709
YOOJIYUN <ys7638@naver.com>
2021-05-20 13:59:31 +0900

    fix: User model to add introduce default

--- src/Models/User/model.ts
+++ src/Models/User/model.ts
@@ -6,7 +6,7 @@ const UserSchema = new Schema(
     name: { type: String, required: true },
     phone: { type: String, required: true },
     role: { type: String, required: true },
-    introduce: { type: String },
+    introduce: { type: String, default: "" },
     userImg: { type: String },
   },
   {
