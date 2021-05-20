edfdebeceaa36c63dc3f7b29df290601faf2fa7b
kks <keumks789@naver.com>
2021-05-20 21:03:35 +0900

    fix : 채팅방 삭제 및 채팅방 삭제 시 작동하는 api

--- src/Models/Room/model.ts
+++ src/Models/Room/model.ts
@@ -1,24 +1,25 @@
-import Room from "./interface";
-import { Schema, model, Model, Types } from "mongoose";
+import Room from "./interface"
+import { Schema, model, Model, Types } from "mongoose"
 
 const RoomSchema = new Schema(
   {
     admin: { type: Types.ObjectId, ref: "user", required: true },
     post: { type: Types.ObjectId, ref: "post", required: true },
     participant: { type: Types.ObjectId, ref: "user", required: true },
+    removeCheck: { type: [{ type: Types.ObjectId, ref: "user" }] },
   },
   { timestamps: true }
-);
+)
 
 RoomSchema.virtual("lastChat", {
   ref: "chat",
   localField: "_id", // roomSchema에서 참조하는 것
   foreignField: "roomId", // chatSchema에서 참자하는 것이다.
   options: { sort: { createdAt: -1 }, perDocumentLimit: 1 },
-});
+})
 
-RoomSchema.set("toObject", { virtuals: true });
-RoomSchema.set("toJSON", { virtuals: true });
+RoomSchema.set("toObject", { virtuals: true })
+RoomSchema.set("toJSON", { virtuals: true })
 
-const RoomModel: Model<Room> = model("room", RoomSchema);
-export default RoomModel;
+const RoomModel: Model<Room> = model("room", RoomSchema)
+export default RoomModel
--- src/routers/Chat/roomService.ts
+++ src/routers/Chat/roomService.ts
@@ -1,12 +1,15 @@
-import { Room, RoomModel } from "../../models/Room";
-import { Post, PostModel } from "../../models/Post";
-import { Chat, ChatModel } from "../../models/Chat";
-import { userInfo } from "../../routers/config";
+import { Room, RoomModel } from "../../models/Room"
+import { Post, PostModel } from "../../models/Post"
+import { ChatModel } from "../../models/Chat"
+import { userInfo } from "../../routers/config"
 
+interface filterRoom extends Room {
+  lastChat?: string[]
+}
 class RoomService {
-  private roomModel = RoomModel;
-  private postModel = PostModel;
-  private chatModel = ChatModel;
+  private roomModel = RoomModel
+  private postModel = PostModel
+  private chatModel = ChatModel
   constructor() {}
 
   createRoom = async (roomData: Room, userId: string): Promise<string> => {
@@ -14,80 +17,77 @@ class RoomService {
     const post = await this.postModel.findOne({
       _id: roomData.post,
       user: roomData.admin,
-    });
-    if (!post) throw new Error("잘못된 정보가 기재되었습니다.");
+    })
+    if (!post) throw new Error("잘못된 정보가 기재되었습니다.")
 
     // 자신의 게시물에는 채팅방X
-    if (roomData.admin === userId)
-      throw new Error("자신의 게시물에는 채팅이 불가능합니다.");
+    if (roomData.admin === userId) throw new Error("자신의 게시물에는 채팅이 불가능합니다.")
 
     //이미 채팅방이 생성된 경우
     const room = await this.roomModel.findOne({
       admin: roomData.admin,
       participant: userId,
       post: roomData.post,
-    });
-    if (room) return room._id;
+    })
+    if (room) return room._id
 
     //새롭게 채팅방 생성하는 경우
     const newRoom = new this.roomModel({
       post: roomData.post,
       admin: roomData.admin,
       participant: userId,
-    });
-    await newRoom.save();
-    return newRoom._id;
-  };
+    })
+    await newRoom.save()
+    return newRoom._id
+  }
 
-  getRooms = async (userId: string): Promise<Room[]> => {
-    const rooms = await this.roomModel
+  getRooms = async (userId: string): Promise<(filterRoom | undefined)[]> => {
+    const rooms: filterRoom[] = await this.roomModel
       .find({
         $or: [{ admin: userId }, { participant: userId }],
       })
       .populate("participant", userInfo)
       .populate("admin", userInfo)
       .populate("lastChat")
-      .sort("-createdAt");
-    return rooms;
-  };
+      .sort("-createdAt")
 
-  getRoomsLastChat = async (rooms: Room[]): Promise<(Chat | Object)[]> => {
-    let lastChat: (Chat | Object)[] = [];
-    const chats = await this.chatModel.find({}).sort("-createdAt");
-
-    for (let i = 0; i < rooms.length; i++) {
-      let flag = 0;
-      for (let j = 0; j < chats.length; j++) {
-        typeof rooms;
-        if (rooms[i]._id == chats[j].roomId) {
-          lastChat.push(chats[j]);
-          flag = 1;
-          break;
-        }
+    const filterRooms = rooms.map((room) => {
+      if (room.lastChat || !room.removeCheck.includes(userId)) {
+        return room
       }
-      if (flag === 0) {
-        lastChat.push({});
-      }
-    }
-    return lastChat;
-  };
+    })
+    return filterRooms
+  }
 
   getRoomById = async (roomId: string): Promise<Room | null> => {
     const room = await this.roomModel
       .findById(roomId)
       .populate("participant", userInfo)
       .populate("admin", userInfo)
-      .populate("post", "title");
-    return room;
-  };
+      .populate("post", "title")
+    return room
+  }
+
+  checkRemove = (room: Room): Boolean => {
+    if (room.removeCheck.length > 0) return true
+    return false
+  }
 
   getParticipants = async (roomInfo: Room): Promise<Post | null> => {
     const post = await this.postModel
       .findById(roomInfo.post)
       .populate("participants", userInfo)
-      .select("participants");
-    return post;
-  };
+      .select("participants")
+    return post
+  }
+
+  deleteRoomById = async (roomId: string, userId: string): Promise<Room> => {
+    const room = await this.roomModel.findByIdAndUpdate(roomId, {
+      $push: { removeCheck: userId },
+    })
+    if (!room) throw new Error("해당 방이 존재하지 않습니다.")
+    return room
+  }
 }
 
-export default RoomService;
+export default RoomService
