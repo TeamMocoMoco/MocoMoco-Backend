5773a8e9e6ced9592a8f1b521054ac552e345805
kks <keumks789@naver.com>
2021-05-21 11:16:35 +0900

    add chat test.ts

--- src/Test/unit/RouterTest/posts.test.ts
+++ src/Test/unit/RouterTest/posts.test.ts
@@ -1,22 +1,22 @@
-import mongoose from "mongoose"
-import { dropAllCollections, removeAllCollections } from "../../test-setup"
-import PostService from "../../../routers/Post/postService"
-import { UserModel } from "../../../models/User"
-import { PostModel } from "../../../models/Post"
-const databaseName = "test"
+import mongoose from "mongoose";
+import { removeSelectCollections } from "../../test-setup";
+import PostService from "../../../routers/Post/postService";
+import { UserModel } from "../../../models/User";
+import { PostModel } from "../../../models/Post";
+const databaseName = "test";
 
 beforeAll(async () => {
-  const url = `mongodb://127.0.0.1/${databaseName}`
+  const url = `mongodb://127.0.0.1/${databaseName}`;
   await mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-})
+  });
+});
 
 describe("포스트 서비스 테스트하기", () => {
-  const postService = new PostService()
-  let newPost: any
-  let userId: any
+  const postService = new PostService();
+  let newPost: any;
+  let userId: any;
 
   test("포스트 생성", async () => {
     // arrange
@@ -27,32 +27,28 @@ describe("포스트 서비스 테스트하기", () => {
       meeting: "online",
       category: "모각코",
       status: true,
-    }
-    const u1 = {
-      name: "kkk",
-      phone: "k",
-      role: "개발자",
-    }
-    const user1 = await UserModel.create(u1)
-    userId = user1._id
+    };
+
+    const user1 = await UserModel.create({ name: "test" });
+    userId = user1._id;
 
     // act
-    newPost = await postService.createPost(post, userId)
-    const findPost = await PostModel.findById(newPost._id)
+    newPost = await postService.createPost(post, userId);
+    const findPost = await PostModel.findById(newPost._id);
     // assert
-    expect(newPost._id).toEqual(findPost!._id)
-  })
+    expect(newPost._id).toEqual(findPost!._id);
+  });
 
   test("포스트 가지고오기 ", async () => {
     // act
-    const post = await postService.getPostById(newPost._id)
+    const post = await postService.getPostById(newPost._id);
 
     //assert
-    expect(post!._id).toEqual(newPost._id)
-    expect(post!.title).toStrictEqual("test")
-    expect(typeof post!.title).toBe("string")
-    expect(typeof post!.personnel).toBe("number")
-  })
+    expect(post!._id).toEqual(newPost._id);
+    expect(post!.title).toStrictEqual("test");
+    expect(typeof post!.title).toBe("string");
+    expect(typeof post!.personnel).toBe("number");
+  });
 
   test("포스트 업데이트 하기", async () => {
     // arrange
@@ -63,71 +59,61 @@ describe("포스트 서비스 테스트하기", () => {
       meeting: "offline",
       category: "프로젝트",
       status: false,
-    }
+    };
     // act
-    const postId = newPost._id.toHexString()
-    newPost = await postService.updatePost(postUpdateData, userId, postId)
+    const postId = newPost._id.toHexString();
+    newPost = await postService.updatePost(postUpdateData, userId, postId);
     // assert
-    expect(newPost!.title).toStrictEqual("update test")
-  })
+    expect(newPost!.title).toStrictEqual("update test");
+  });
 
   test("스터디에 추가하기", async () => {
     // arrange
+    const user2 = await UserModel.findOne({ name: "test1" });
+    const user3 = await UserModel.findOne({ name: "test2" });
+    const newID = mongoose.Types.ObjectId().toHexString();
 
-    const u2 = {
-      name: "hhh",
-      phone: "h",
-      role: "기획자",
-    }
+    if (!user2) return;
 
-    const u3 = {
-      name: "zzz",
-      phone: "z",
-      role: "디자이너",
-    }
-
-    const user2 = await UserModel.create(u2)
-    const user3 = await UserModel.create(u3)
-    const newID = mongoose.Types.ObjectId().toHexString()
     // act
-    const postId = newPost._id.toHexString()
-    await postService.addParticipant(postId, userId, user2._id)
-    const findPost = await PostModel.findById(postId)
+    const postId = newPost._id.toHexString();
+    await postService.addParticipant(postId, userId, user2._id);
+    const findPost = await PostModel.findById(postId);
 
     // assert
-    expect(findPost!.participants).toContainEqual(user2._id)
+    expect(findPost!.participants).toContainEqual(user2._id);
 
     // participant가 잘못된 정보일 때
     expect(async () => {
-      await postService.addParticipant(postId, userId, newID)
-    }).rejects.toThrow("잘못된 참가자 정보입니다.")
+      await postService.addParticipant(postId, userId, newID);
+    }).rejects.toThrow("잘못된 참가자 정보입니다.");
 
     // postId 또는 userId가 잘못 되었을 때
     expect(async () => {
-      await postService.addParticipant(postId, newID, user2._id)
-    }).rejects.toThrow("잘못된 정보가 기재되었습니다.")
+      await postService.addParticipant(postId, newID, user2._id);
+    }).rejects.toThrow("잘못된 정보가 기재되었습니다.");
     expect(async () => {
-      await postService.addParticipant(newID, userId, user2._id)
-    }).rejects.toThrow("잘못된 정보가 기재되었습니다.")
+      await postService.addParticipant(newID, userId, user2._id);
+    }).rejects.toThrow("잘못된 정보가 기재되었습니다.");
 
     expect(async () => {
-      await postService.addParticipant(postId, userId.toHexString(), userId)
-    }).rejects.toThrow("본인은 참가자로 넣거나 뺄 수 없습니다.")
-  })
+      await postService.addParticipant(postId, userId.toHexString(), userId);
+    }).rejects.toThrow("본인은 참가자로 넣거나 뺄 수 없습니다.");
+  });
 
   test("포스트 삭제하기", async () => {
     // arrange
 
     // act
-    const postId = newPost._id.toHexString()
-    await postService.deletePost(userId, postId)
-    const post = await postService.getPostById(postId)
+    const postId = newPost._id.toHexString();
+    await postService.deletePost(userId, postId);
+    const post = await postService.getPostById(postId);
     // assert
-    expect(post).toBeNull()
-  })
-})
+    expect(post).toBeNull();
+  });
+});
 
 afterAll(async () => {
-  await dropAllCollections()
-  await mongoose.connection.close()
-})
+  await removeSelectCollections("posts");
+  await mongoose.connection.close();
+});
--- src/Test/unit/RouterTest/user.test.ts
+++ src/Test/unit/RouterTest/user.test.ts
@@ -1,54 +1,54 @@
-import mongoose from "mongoose"
-import { dropAllCollections, removeAllCollections } from "../../test-setup"
-import UserService from "../../../routers/User/userService"
-import { User } from "../../../models/User"
-import "dotenv/config"
-const databaseName = "test"
+import mongoose from "mongoose";
+import { removeSelectCollections } from "../../test-setup";
+import UserService from "../../../routers/User/userService";
+import { User } from "../../../models/User";
+import "dotenv/config";
+const databaseName = "test";
 
 const me: any = {
-  name: "kks",
-  phone: "01077775555",
+  name: "test1",
+  phone: "01012345678",
   role: "개발자",
-}
+};
 
 const you: any = {
-  name: "hhs",
-  phone: "01011112222",
+  name: "test2",
+  phone: "01056781234",
   role: "개발자",
-}
+};
 
 beforeAll(async () => {
-  const url = `mongodb://127.0.0.1/${databaseName}`
-  await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-})
+  const url = `mongodb://127.0.0.1/${databaseName}`;
+  await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+});
 
 describe("유저 생성 및 유저 로그인 기능 테스트 하기", () => {
-  const userService = new UserService()
+  const userService = new UserService();
 
   test("유저 생성", async () => {
     //act
-    const newUser = await userService.createUser(me, me.phone)
-    const newUser2 = await userService.createUser(you, you.phone)
+    const newUser = await userService.createUser(me, me.phone);
+    const newUser2 = await userService.createUser(you, you.phone);
     // assert
-    expect(newUser.phone).toEqual(me.phone)
-  })
+    expect(newUser.phone).toEqual(me.phone);
+  });
 
   test("로그인", async () => {
     //act
-    const result = await userService.login(me.phone)
-    const meToken: string = result.token
-    const meUser: User = result.user
+    const result = await userService.login(me.phone);
+    const meToken: string = result.token;
+    const meUser: User = result.user;
 
     //assert
-    expect(me.phone).toEqual(meUser.phone)
-    expect(me.phone).not.toEqual(you.phone)
+    expect(me.phone).toEqual(meUser.phone);
+    expect(me.phone).not.toEqual(you.phone);
     expect(async () => {
-      await userService.login("000")
-    }).rejects.toThrow("없는 휴대폰 번호입니다")
-  })
-})
+      await userService.login("000");
+    }).rejects.toThrow("없는 휴대폰 번호입니다");
+  });
+});
 
 afterAll(async () => {
-  await dropAllCollections()
-  await mongoose.connection.close()
-})
+  await removeSelectCollections("users");
+  await mongoose.connection.close();
+});
